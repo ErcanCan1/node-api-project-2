099a469c3f9d18dbc1c9e77dd723d6212acfc4d2
Ercan <ercann.6191@gmail.com>
2024-05-20 14:08:51 +0300

    another commit

--- api/posts/posts-router.js
+++ api/posts/posts-router.js
@@ -23,7 +23,7 @@ router.get("/:id", (req,res) => {//params urlnin sonuna yazılan değer
     });
 });
 
-router.post("/", (req, res) => {//body url nin içeriği
+/*router.post("/", (req, res) => {//body url nin içeriği
     const {title, contents} = req.body;
     if(!title || !contents){ 
         res.status(400).json({ message: "Gönderi için bir title ve content sağlayın"});
@@ -38,6 +38,78 @@ router.post("/", (req, res) => {//body url nin içeriği
            
         } 
     }
-)
+)*/
 
+router.post("/", async (req, res) => {//yukardaki postun async await li hali
+    const {title, contents} = req.body;
+    if(!title || !contents){ 
+        res.status(400).json({ message: "Gönderi için bir title ve content sağlayın"});
+    }else{
+        try {
+            let response= await Post.insert({title, contents});
+            let insertPost = await Post.findById(respnde.id);
+            res.status(201).json(insertPost);
+        } catch (error) {
+            res.status(5000).json({message:"Veritabanına kaydedilemedi"});
+        }
+        Post.insert({title,contents}).then(({id}) => {
+            Post.findById(id).then(findedPost => {
+                res.status(201).json(findedPost);
+            });
+        }).catch( err => {
+            res.status(5000).json({message:"Veritabanına kaydedilemedi"});
+        })
+           
+        } 
+    })
+
+    router.put("/:id", async (req, res) =>{
+        let existPost = await Post.findById(req.params.id);
+        if(!existPost){
+            res.status(500).json({message:"Belirtilen id li gönderi bulunamadı"});
+        } else{
+            let { title, contents} =req.body;
+            if(!title || !contents){
+                res.status(400).json({message:"Gönderi için title ve content sağlayın"});
+            }else{
+                try {
+                    let updatedPostId = await Post.update(req.params.id, req.body);
+                    let updatedPost = await Post.findById(updatedPostId);
+                    res.status(200).json(updatedPost);
+                } catch (error) {
+                    res.status(500).json({message: "Gönderi bilgileri güncellenemedi"});
+                }
+            
+            }
+        }
+    });
+
+    router.delete("/:id", async (req, res) =>{
+        try {
+            let existPost = await Post.findById(req.params.id);
+            if(!existPost){
+                res.status(404).json({message: "Belirtilen id li post bulunamadı"});
+            }else{
+                await Post.remove(req.params.id);
+                res.status(200).json(existPost);
+            }
+        } catch (e) {
+            res.status(500).json({message: "Gönderi silinemedi"});
+        }
+    })
+
+    router.get("/:id/comments", async (req, res) => {
+        try {
+            let existPost = await Post.findById(req.params.id);
+            if(!existPost){
+                res.status(404).json({message:"Girilen id'li gönderi bulunamadı"});
+            }else {
+                let comments = await Post.findPostComments(req.params.id);
+                res.status(200).json(comments);
+            }
+        } catch (error) {
+            res.status(500).json({message: "belirtilen id li cımmente ulaşılamadı"});
+        }
+    })
+    
 module.exports = router;
