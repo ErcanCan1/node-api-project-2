e9c803542f5c33c0cc0b4c49fb9a74e0033264e5
Ercan <ercann.6191@gmail.com>
2024-05-20 13:02:17 +0300

    first commit

--- api/posts/posts-router.js
+++ api/posts/posts-router.js
@@ -1,1 +1,43 @@
 // posts için gerekli routerları buraya yazın
+const express = require("express");
+const Post = require("./posts-model.js");
+const router = express.Router();
+
+router.get("/", (req, res) => {
+    Post.find().then(posts => {
+        res.json(posts);
+    }).catch(err => {
+        res.status(500).json({message: "Gönderiler alınamadı"});
+    })
+});
+
+router.get("/:id", (req,res) => {//params urlnin sonuna yazılan değer
+    Post.findById(req.params.id).then(findPost => {
+        if(!findPost){
+            res.status(404).json({ message: "ID bulunamadı"});
+        }else {
+            res.json(findPost);
+        }
+    }).catch(err => {
+        res.status(500).json({ message: "Gönderi bilgisi alınamadı"});
+    });
+});
+
+router.post("/", (req, res) => {//body url nin içeriği
+    const {title, contents} = req.body;
+    if(!title || !contents){ 
+        res.status(400).json({ message: "Gönderi için bir title ve content sağlayın"});
+    }else{
+        Post.insert({title,contents}).then(({id}) => {
+            Post.findById(id).then(findedPost => {
+                res.status(201).json(findedPost);
+            });
+        }).catch( err => {
+            res.status(5000).json({message:"Veritabanına kaydedilemedi"});
+        })
+           
+        } 
+    }
+)
+
+module.exports = router;
--- api/server.js
+++ api/server.js
@@ -1,3 +1,9 @@
 // server için gerekli olanları burada ayarlayın
-
+const express = require("express");
+const server = express();
+server.use(express.json());
 // posts router'ını buraya require edin ve bağlayın
+const router = require("./posts/posts-router.js");
+server.use("/api/posts", router);
+
+module.exports =server;
